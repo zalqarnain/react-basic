c4c1e69754b0bd79b46c636d3ff67350ac2f6a7c
zalqarnain <zalqarnainahsen03@gmail.com>
2023-12-26 23:05:51 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,71 +1,14 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Container, Grid, Paper, Typography, Button } from '@mui/material';
-import Navbar from './Navbar';
-import ProductList from './ProductList';
-import Cart from './Cart';
-import Home from './Home';
-import Footer from './Footer';
+import React from 'react';
+import Counter from './Counter';
 
-const App = () => {
-  //cart state
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, { id: product.id, name: product.name, price: product.price }]);
-  };
-
-  const removeFromCart = (itemId) => {
-    setCart((prevCart) => prevCart.filter((item) => item.id !== itemId));
-  };
-
-  // Dummy product data
-  const laptops = [
-    { id: 1, name: 'HP Laptop', description: 'Powerful Performance, Sleek Design', price: 999.99 },
-    { id: 2, name: 'Lenovo Laptop', description: 'Elegance in Every Click', price: 899.99 },
-  ];
-
-  
-  const mobiles = [
-    { id: 1, name: 'Samsung Mobile', description: 'Innovation in Hand', price: 299.99 },
-    { id: 2, name: 'iPhone Mobile', description: 'Elegance in Innovation', price: 499.99 },
-  ];
-
-  const tablets = [
-    { id: 1, name: 'Huawei Tablet', description: 'Power in Portability', price: 199.99 },
-    { id: 2, name: 'Lenovo Tablet', description: 'Unleash Your Imagination', price: 299.99 },
-  ];
+function App(props) {
 
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-      <Route
-          path="/"
-          element={<Home/>}
-        />
-
-        <Route
-          path="/laptops"
-          element={<ProductList products={laptops} addToCart={addToCart} />}
-        />
-        <Route
-          path="/mobiles"
-          element={<ProductList products={mobiles} addToCart={addToCart} />}
-        />
-        <Route
-          path="/tablets"
-          element={<ProductList products={tablets} addToCart={addToCart} />}
-        />
-        <Route
-          path="/cart"
-          element={<Cart cart={cart} removeFromCart={removeFromCart} />}
-        />
-      </Routes>
-      <Footer />
-
-    </Router>
+    <div>
+      <p>Hello</p>
+      <Counter />
+    </div>
   );
-};
+}
 
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Counter = ({ count, increment, decrement }) => {
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: 'INCREMENT' }),
+    decrement: () => dispatch({ type: 'DECREMENT' }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
